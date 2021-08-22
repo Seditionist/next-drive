b672c1d1d1a99b0dd086b8fabf80f1d9a6de4012
JudeBoy <judegiordano@gmail.com>
2021-08-22 00:54:22 -0400

    Backend Updates
    Co-authored-by: Seditionist <Seditionist@users.noreply.github.com>

---
+++ next-drive-frontend/pages/_app.ts
@@ -0,0 +1,10 @@
+import { AppProps } from "next/dist/shared/lib/router/router"
+import React from "react"
+
+import '../styles/globals.css'
+
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
